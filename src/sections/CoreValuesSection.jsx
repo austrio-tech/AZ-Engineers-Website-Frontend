93af13b8ddcda2ed00e30387a949e29fb29cd881
M-Usman-Tahir <m.usman.tahir.336@gmail.com>
2024-08-01 15:49:02 +0400

    `Updated`: CoreValuesSection

--- src/sections/CoreValuesSection.jsx
+++ src/sections/CoreValuesSection.jsx
@@ -53,37 +53,48 @@ const CardComponent = () => {
   }, []);
 
   return (
-    <div className="main">
-      {cards?.map((card, idx) => {
-        const rotationAngle = 0.5 * (-(cards.length / 2) + card.index + 1);
-        const translateX = 30 * (card.index - cards.length / 2);
-        const translateY = 60 * (card.index - cards.length / 2);
+    <div className="main-container">
+      <div className="header-container">Our <div className="headingTitle">Core Values</div></div>
+      <div className="cards-container">
+        {cards?.map((card, idx) => {
+          const rotationAngle = 0.5 * (-(cards.length / 2) + card.index + 1);
+          const translateX = 30 * (card.index - cards.length / 2);
+          const translateY = 60 * (card.index - cards.length / 2);
 
-        return (
-          <div
-            key={card.n}
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            className={`card ${hovered ? "hovered" : ""}`}
-            style={{
-              backgroundColor: colors[colors.length - card.index - 1],
-              transform: hovered
-                ? `translateX(${translateX}px) translateY(${translateY}px) rotate(${rotationAngle}deg)`
-                : `translateX(${translateX / 10}px) translateY(${
-                    translateY / 10
-                  }px) rotate(${rotationAngle / 10}deg)`,
-              zIndex: hovered ? cards.length - 1 : card.index,
-              transition:
-                "transform 0.3s ease-in-out, z-index 1s ease-in-out, box-shadow 0.3s",
-            }}
-            onClick={() => handleCardClick(card.index)}
-          >
-            <h3 className="cardNumber">{card.number}</h3>
-            <h2 className="cardTitle">{card.title}</h2>
-            <p>{card.description}</p>
-          </div>
-        );
-      })}
+          return (
+            <div
+              key={card.number}
+              onMouseEnter={handleMouseEnter}
+              onMouseLeave={handleMouseLeave}
+              className={`card ${hovered ? "hovered" : ""}`}
+              style={{
+                backgroundColor:
+                  colors[
+                    colors.length -
+                      card.index -
+                      1 -
+                      (colors.length - cards.length)
+                  ],
+                transform: hovered
+                  ? `translateX(${translateX}px) translateY(${translateY}px) rotate(${rotationAngle}deg)`
+                  : `translateX(${translateX / 10}px) translateY(${
+                      translateY / 10
+                    }px) rotate(${rotationAngle / 10}deg)`,
+                zIndex: hovered ? cards.length - 1 : card.index,
+                transition:
+                  "transform 0.3s ease-in-out, z-index 0.3s ease, box-shadow 0.3s",
+              }}
+              onClick={() => handleCardClick(card.index)}
+            >
+              <div className="cardHead">
+                <h3 className="cardNumber">{card.number}</h3>
+                <h2 className="cardTitle">{card.title}</h2>
+              </div>
+              <p className="cardDescription">{card.description}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/variables/vars.jsx
+++ src/variables/vars.jsx
@@ -1,32 +1,37 @@
 export const coreValues = [
   {
-    number: "01",
-    title: "Quality and Speed",
+    number: "05",
+    title: "Integrity",
     description:
-      "Springing from humble roots, the company’s biggest strength remains timely delivery with the best possible quality standards. At all levels of our organization, there is close monitoring on the daily (and sometimes even hourly) progress of works at the site level. The targets are set stringently and followed meticulously.", index:0,
+      "We remain committed to achieving corporate governance standards and ethical business practices that meet the highest possible levels of integrity and compliance, no matter what the cost. Compromising on ethics is most definitely a no-go.",
+    index: 4,
   },
   {
-    number: "02",
-    title: "Safety",
+    number: "04",
+    title: "Quality and Speed",
     description:
-      "We maintain an ever-increasing emphasis on workplace safety. Our company promotes a culture of caring- for those working with us, around us, for our environment and for our society. We try to be at the forefront of implementing and even developing safety best practices.",index:1,
+      "Springing from humble roots, the company’s biggest strength remains timely delivery with the best possible quality standards. At all levels of our organization, there is close monitoring on the daily (and sometimes even hourly) progress of works at the site level. The targets are set stringently and followed meticulously.",
+    index: 3,
   },
   {
     number: "03",
     title: "Sustainability",
     description:
-      "We leave a positive legacy for the people we work with, the communities we work in, and the world in which we operate. We want to enhance our impact on the environment, working with our supply chain partners, customers and communities to ensure our choices are sustainable.",index:2,
+      "We leave a positive legacy for the people we work with, the communities we work in, and the world in which we operate. We want to enhance our impact on the environment, working with our supply chain partners, customers and communities to ensure our choices are sustainable.",
+    index: 2,
   },
   {
-    number: "04",
+    number: "02",
+    title: "Safety",
+    description:
+      "We maintain an ever-increasing emphasis on workplace safety. Our company promotes a culture of caring- for those working with us, around us, for our environment and for our society. We try to be at the forefront of implementing and even developing safety best practices.",
+    index: 1,
+  },
+  {
+    number: "01",
     title: "Cost-competitive innovation",
     description:
-      "Our clients demand cost-competitive solutions. Every day we strive to establish methods and procedures aimed at increasing our productivity at site and in the office. We continually invest in new tools that allow us to drive cost and schedule certainty and to optimize efficiency. Our connections with the most cost-efficient suppliers globally enable our clients to enjoy the most cost-effective solution without having to compromise on quality.",index:3,
-  },
-  {
-    number: "05",
-    title: "Integrity",
-    description:
-      "We remain committed to achieving corporate governance standards and ethical business practices that meet the highest possible levels of integrity and compliance, no matter what the cost. Compromising on ethics is most definitely a no-go.",index:4,
+      "Our clients demand cost-competitive solutions. Every day we strive to establish methods and procedures aimed at increasing our productivity at site and in the office. We continually invest in new tools that allow us to drive cost and schedule certainty and to optimize efficiency. Our connections with the most cost-efficient suppliers globally enable our clients to enjoy the most cost-effective solution without having to compromise on quality.",
+    index: 0,
   },
 ];
